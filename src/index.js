28dc6cf2294d27062440e00c192a7dd214437421
akshit <akshitrathore55128@gmail.com>
2025-02-15 23:47:41 +0530

    learn database connection

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv"
+import express from "express"
+import { dbconnect } from "../db/database_connect.js"
+
+dotenv.config({
+    path: './.env'
+})
+dbconnect()
+// const app = express();
+
+// ; (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error", (error) => {
+//             console.log("error", error)
+//             throw error
+//         })
+//         app.listen(3000, console.log("Success"))
+//     }
+//     catch (error) {
+//         console.log("error hora bhai database connection me", error);
+//         throw error
+//     }
+// })()
